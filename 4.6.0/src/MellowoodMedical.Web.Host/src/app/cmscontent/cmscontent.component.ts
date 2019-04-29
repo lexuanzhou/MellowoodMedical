5af15e51265e8c59ba8fb2cae77300bebcdfd1af
Lexuan Zhou <zhoulexuan123@gmail.com>
2019-04-29 17:08:12 -0400

    add initial cmscontent feature frontend files

---
+++ 4.6.0/src/MellowoodMedical.Web.Host/src/app/cmscontent/cmscontent.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, Injector } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+
+@Component({
+    templateUrl: './cmscontent.component.html'
+})
+
+export class CMSContentComponent extends AppComponentBase {
+    constructor(
+        injector: Injector
+    ) {
+        super(injector);
+    }
+}
